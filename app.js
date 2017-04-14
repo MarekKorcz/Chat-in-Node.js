0fcf133e97ce53b35c6a87163d385519ba8a5b71
Marek Korcz <mark.korcz@gmail.com>
2017-04-14 23:42:25 +0200

    Minor change in port that app is listening

--- app.js
+++ app.js
@@ -3,8 +3,17 @@ var express = require('express'),
         server = require('http').createServer(app),
         io = require('socket.io').listen(server);
 
-server.listen(3000);
+server.listen(3301);
 
 app.get('/', function(req, res){
    res.sendFile(__dirname + '/index.html'); 
+});
+
+io.sockets.on('connection', function(socket){
+    
+    
+    socket.on('send-message', function(data){
+        
+        io.sockets.emit('new message', data);
+    });
 });
