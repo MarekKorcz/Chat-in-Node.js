cd822358e1d35217a6d4c5ef5dd3174dd7551679
Marek Korcz <mark.korcz@gmail.com>
2017-04-15 23:50:32 +0200

    Adding users to chat. Server side. A bug with displaying users list

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 var express = require('express'),
         app = express(),
         server = require('http').createServer(app),
-        io = require('socket.io').listen(server);
+        io = require('socket.io').listen(server),
+        nicknames = [];
 
 server.listen(3301);
 
@@ -11,9 +12,32 @@ app.get('/', function(req, res){
 
 io.sockets.on('connection', function(socket){
     
+    function updateNicknames(){
+        io.sockets.emit('usernames', nicknames);
+    }
+    
+    socket.on('new user', function(data, callback){
+       
+        if(nicknames.indexOf(data) != -1){
+           callback(false);
+       }else{
+           callback(true);
+           socket.nickname = data;
+           nicknames.push(socket.nickname);
+           io.sockets.emit('usernames', nicknames);
+           updateNicknames();
+       }        
+    });
     
     socket.on('send-message', function(data){
         
-        io.sockets.emit('new message', data);
+        io.sockets.emit('new message', {msg: data, nick: socket.nickname});
+    });
+    
+    socket.on('disconnect', function(data){
+        if(!socket.nickname) return;
+        
+        nicknames.splice(nicknames.indexOf(socket.nicknames), 1);
+        updateNicknames();
     });
 });
